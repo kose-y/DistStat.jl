88bbffcbc3ca78af8e780d136827d31032cf8324
Seyoon <syko0507@snu.ac.kr>
2019-08-29 16:59:45 +0900

    minor fix in bcasting

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -96,7 +96,7 @@ end
 bclocal(x::T, idxs) where T = _bclocal(BroadcastStyle(T), x, idxs)
 function _bclocal(::MPIArrayStyle, x, idxs)
     bcidxs = _bcview(axes(x), idxs)
-    @assert all(bcidxs .== x.partitioning[Rank() + 1])
+    @assert all(map( (y, z) -> (y  == z) || length(z) == 1 , bcidxs, x.partitioning[Rank() + 1]))
     x.localarray
     # makelocal(x, bcidxs...) # TODO: makelocal
 end
