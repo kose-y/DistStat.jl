6c481cb98bdccdbe5033d14878705f637dfb79d6
Seungbongjung <54697679+Seungbongjung@users.noreply.github.com>
2019-11-01 12:59:43 +0900

    test runs well
    except when a is CuArray

--- test/test_opnorm.jl
+++ test/test_opnorm.jl
@@ -1,38 +1,18 @@
-using DistStat, LinearAlgebra,Random,Test
+using DistStat, LinearAlgebra,Random,Test, Pkg
 
-types=[Float64,Float32]
+type=[Float64,Float32]
 if haskey(Pkg.installed(), "CuArrays")
     using CuArrays
-    ArrayType = CuArray
+    A= CuArray
 else
-    ArrayType = Array
+    A = Array
 end
 
 
 for T in type
-    a=rand(7,7)
+    a=Array{T}(reshape(collect(1:81),9,9))
     a_dist=distribute(a)
-    cols=a_dist.partitioning[DistStat.Rank()+1][2]
-    n=div(7,DistStat.Rank()+1)
-    m=rem(7,DistStat.Rank()+1)
-    ans=Vector{Int64}(undef,DistStat.Rank()+1)
-    for i in 1:(DistStat.Rank()+1)
-        if (i <= DistStat.Rank())
-            ans[i]=n+m
-        else
-            ans[i]=n
-        end
-    end
 
-    result=Vector{Float64}(undef,DistStat.Rank()+1)
-    for i in 1:(DistStat.Rank()+1)
-        if (i==1)
-            result=opnorm(a[:,1:ans[i]])
-        else
-            result=opnorm(a[:,(ans[i-1]+1):ans[i]])
-        end
-    end
-
-    println(@test all(isapprox(opnorm(a_dist),result)))
+    println(@test isapprox(opnorm(a),opnorm(a_dist)))
 
 end
