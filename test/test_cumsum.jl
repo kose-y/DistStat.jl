6cc377bed86f90f80144f735f0b0ce4f95a92bb5
Seungbongjung <54697679+Seungbongjung@users.noreply.github.com>
2019-11-01 12:18:51 +0900

    test runs well under certain condition
    When type of a is CuArray, test doesn't run well. I think solving this problem is sufficient.

---
+++ test/test_cumsum.jl
@@ -0,0 +1,20 @@
+using DistStat, Pkg, Random, Test
+
+type=[Float64,Float32]
+
+if haskey(Pkg.installed(), "CuArrays")
+    using CuArrays
+    A = CuArray
+else
+    A = Array
+end
+
+for T in type
+    a = Array{T}(reshape(collect(1:49),7,7))
+    a_dist = distribute(a)
+    cols=a_dist.partitioning[DistStat.Rank()+1][2]
+
+    println(@test cumsum(a,dims=1)[:,cols]==cumsum(a_dist,dims=1).localarray)
+    println(@test cumsum(a,dims=2)[:,cols]==cumsum(a_dist,dims=2).localarray)
+
+end
