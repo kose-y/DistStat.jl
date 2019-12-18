1d523e5b82cac2cf12b4046d01ce65c6a83dd5d3
Seungbongjung <54697679+Seungbongjung@users.noreply.github.com>
2019-12-18 22:40:30 +0900

    Create test_mul2.jl

---
+++ test/test_mul2.jl
@@ -0,0 +1,37 @@
+using DistStat, LinearAlgebra, Test, Random
+
+type=[Float32,Float64]
+
+if get(ENV,"JULIA_MPI_TEST_ARRAYTYPE","") == "CuArray"
+    using CuArrays
+    ArrayType = CuArray
+else
+    ArrayType = Array
+end
+
+for T in type
+
+    A=ArrayType{T}(reshape(collect(1:63),7,9))
+    B=ArrayType{T}(reshape(collect(-31:31),7,9))
+    A_dist=distribute(A)
+    B_dist=distribute(B)
+    B_distt=distribute(ArrayType{T}(transpose(B)))
+
+    C=MPIMatrix{T,ArrayType}(undef,9,9)
+    cols1=B_dist.partitioning[DistStat.Rank()+1][2]
+    cols2=B_distt.partitioning[DistStat.Rank()+1][2]
+
+    result1=LinearAlgebra.mul!(C,transpose(A),B_dist)
+    ans1=transpose(A)*B
+
+    println(@test isapprox(result1.localarray,ans1[:,cols1]))
+
+    result2=LinearAlgebra.mul!(transpose(C),transpose(A_dist),B)
+    println(@test isapprox(result2.localarray,ArrayType{T}(transpose(ans1))[:,cols1]))
+
+    D=Vector{Float64}(reshape()
+    E=Vector{Float64}(undef,9)
+    LinearAlgebra.mul!(E,transpose(A_dist),D)
+    println(@test isapprox(E,transpose(A)*D)
+
+end
