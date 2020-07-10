32c47936e01e867e0ffc29e538324106bc88edc9
Seyoon Ko <syko0507@snu.ac.kr>
2020-07-10 15:09:28 +0900

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,21 @@
 using Documenter, DistStat
 
-makedocs(sitename="Dist Stat.")
+makedocs(
+    format = Documenter.HTML(),
+    sitename = "DistStat.jl",
+    authors = "Seyoon Ko",
+    clean = true,
+    debug = true,
+    pages = [
+        "index.md"
+    ]
+)
+
+deploydocs(
+    repo   = "github.com/kose-y/DistStat.jl.git",
+    target = "build",
+    deps   = nothing,
+    make   = nothing
+)
 
 
