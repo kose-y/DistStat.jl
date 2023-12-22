f9f1a415d72ace35ff51e73e44a5a0127bb2a602
Seyoon Ko <kos@ucla.edu>
2023-12-21 20:55:00 -0800

    fix constant name

--- src/cuda.jl
+++ src/cuda.jl
@@ -1,7 +1,7 @@
 using .CUDA
 
 function set_device!()
-    lcomm = MPI.Comm_split_type(COMM_WORLD, MPI.MPI_COMM_TYPE_SHARED, MPI.Comm_rank(COMM_WORLD))
+    lcomm = MPI.Comm_split_type(COMM_WORLD, MPI.COMM_TYPE_SHARED, MPI.Comm_rank(COMM_WORLD))
     CUDA.device!(MPI.Comm_rank(lcomm) % length(devices()))
 end
 
